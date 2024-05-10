56dec057f75a3b24b9411793da5fa0302a81eace
Patience <patiencerasebitse@gmail.com>
2024-05-10 15:46:11 +0200

    Added Navbar hamburger menu and fixed button2.

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
-// document.getElementById("downloadButton").addEventListener("click", function() {
-//     console.log("Button clicked!"); // Check if this message appears in the browser console when clicking the button
-//     window.location.href = "documents/Resume.pdf";
-// });
+// Wait for the HTML document to finish loading
+document.addEventListener("DOMContentLoaded", function() {
+    // Find the toggle button element by its ID
+    var toggleButton = document.getElementById("navbar-toggle");
+    // Find the navbar collapse element by its ID
+    var navbarCollapse = document.getElementById("navbar-collapse");
 
+    // Add a click event listener to the toggle button
+    toggleButton.addEventListener("click", function() {
+        // Toggle the "collapsed" class on the navbar collapse element
+        navbarCollapse.classList.toggle("collapsed");
+        // Toggle the "collapsed" class on the toggle button itself
+        toggleButton.classList.toggle("collapsed");
+    });
+});
