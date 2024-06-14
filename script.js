549f0ed19ba4832beb3ec96c54623e4e957b6eb7
Patience <patiencerasebitse@gmail.com>
2024-06-14 14:40:41 +0200

    Added a new intro with Typing effect .

--- script.js
+++ script.js
@@ -13,3 +13,42 @@ document.addEventListener("DOMContentLoaded", function() {
         toggleButton.classList.toggle("collapsed");
     });
 });
+
+
+const introText1 = "Hi there, I'm";
+const introText2 = "Patience Olebogeng Rasebitse";
+const introText3 = "and I'm an aspiring Software Developer";
+
+let index1 = 0;
+let index2 = 0;
+let index3 = 0;
+
+function typeIntro1() {
+    if (index1 < introText1.length) {
+        document.getElementById('intro1').innerHTML += introText1.charAt(index1);
+        index1++;
+        setTimeout(typeIntro1, 100);
+    } else {
+        setTimeout(typeIntro2, 500); // Wait before starting the next part
+    }
+}
+
+function typeIntro2() {
+    if (index2 < introText2.length) {
+        document.getElementById('intro2').innerHTML += introText2.charAt(index2);
+        index2++;
+        setTimeout(typeIntro2, 100);
+    } else {
+        setTimeout(typeIntro3, 500); // Wait before starting the next part
+    }
+}
+
+function typeIntro3() {
+    if (index3 < introText3.length) {
+        document.getElementById('intro3').innerHTML += introText3.charAt(index3);
+        index3++;
+        setTimeout(typeIntro3, 100);
+    }
+}
+
+document.addEventListener('DOMContentLoaded', typeIntro1);
